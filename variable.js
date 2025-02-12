44418a6b6bd0861ac4c8465cd93530e2f5e13c36
sonatanpaul <iamsonatanpaul@gmail.com>
2025-02-12 18:16:47 +0600

    learn number data type

--- variable.js
+++ variable.js
@@ -15,7 +15,19 @@ let isPoor = true;
 let footballPrice = 1500;
 
 // string type variable
-let name = "Sonatan";
+let names = "Sonatan";
 
 // boolean type variable
 let isPassed = true;
+
+// Learn Number Data Type
+
+let MangoPrice = 60;
+console.log(MangoPrice);
+MangoPrice += 10;
+console.log(MangoPrice);
+
+let potatoPrice = 50;
+let orangePrice = 250;
+let totalPrice = potatoPrice + orangePrice;
+console.log(totalPrice);

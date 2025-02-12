6eb991f570fc830c865532fb9d381cabc5301718
sonatanpaul <iamsonatanpaul@gmail.com>
2025-02-12 08:49:46 +0600

    learn how to declare variable

--- variable.js
+++ variable.js
@@ -1,1 +1,12 @@
-console.log("Hello World");
+// how to declare variable *******
+
+// using var keyword
+var name = "Hello World";
+
+// using let keyword
+let number = 20;
+
+// using const keyword
+let isPoor = true;
+
+// learn variable type

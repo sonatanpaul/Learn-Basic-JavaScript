ea4d48fed56e6d961597e214e6d2d6191596d23f
sonatanpaul <iamsonatanpaul@gmail.com>
2025-02-14 21:50:42 +0600

    learn for of loop

---
+++ for-of-loop.js
@@ -0,0 +1,22 @@
+const persons = ["Bristi", "Priyonti", "Farjana", "Monira"];
+
+for (const person of persons) {
+  console.log(person);
+}
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+for (const number of numbers) {
+  console.log(number, "Happy Valentine Day");
+}
+
+const friends = ["Akash", "Bikash", "Asraful", "Rayhan"];
+
+for (const friend of friends) {
+  console.log(friend, "Already Passed 10 Year");
+}
+
+const counts = [10, 11, 12, 13, 14, 15, 16];
+for (const count of counts) {
+  console.log(count, "Hello World");
+}

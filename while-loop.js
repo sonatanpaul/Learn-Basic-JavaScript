37b3dbf12a67cb8039d7aaf18142fb6c530b5e92
sonatanpaul <iamsonatanpaul@gmail.com>
2025-02-14 22:52:20 +0600

    learn while loop

---
+++ while-loop.js
@@ -0,0 +1,38 @@
+// learn while loop
+
+// let number = 1;
+
+// while (number <= 50) {
+//   console.log(number);
+//   number++;
+// }
+
+// let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 13];
+// let i = 1;
+
+// while (i < numbers.length) {
+//   console.log(i);
+//   i++;
+// }
+
+// how to check odd  number
+let num = 1;
+
+while (num <= 50) {
+  console.log(num);
+  if (num % 2 == 1) {
+    console.log("odd number", num);
+  }
+  num++;
+}
+
+// how to check even number
+let even = 1;
+
+while (even <= 50) {
+  console.log(even);
+  if (even % 2 == 0) {
+    console.log("even number", even);
+  }
+  even++;
+}

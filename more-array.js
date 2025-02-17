29a7c05df9ad6cc4cdb61f9453d01bb194717aa8
sonatanpaul <iamsonatanpaul@gmail.com>
2025-02-17 23:01:58 +0600

    practice loop with array

---
+++ more-array.js
@@ -0,0 +1,19 @@
+const friends = ["Anik", "Rayhan", "Asraful", "Akash "];
+
+// learn for of loop
+for (const friend of friends) {
+  console.log(friend);
+}
+
+// learn for loop
+for (let i = 0; i < friends.length; i++) {
+  console.log(friends[i]);
+}
+
+// learn while loop
+let i = 0;
+while (i < friends.length) {
+  console.log(friends[i]);
+  i++;
+}
+

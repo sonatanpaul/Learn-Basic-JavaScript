2f03b760a2852575c9d37501959ca273cc8848df
sonatanpaul <iamsonatanpaul@gmail.com>
2025-02-17 12:49:12 +0600

    learn  how to declare string

---
+++ string.js
@@ -0,0 +1,13 @@
+const country = "Bangladesh";
+const division = "Dhaka";
+const district = `B-Baria`;
+const thana = new String("Aminpur");
+// console.log(country);
+// console.log(division);
+// console.log(district);
+// console.log(thana);
+
+console.log(typeof country); // string
+console.log(typeof division); // string
+console.log(typeof district); // string
+console.log(typeof thana); // object but under the hod string

b2390b3b9411abbd57f79d5590a5292d0fdaddcd
sonatanpaul <iamsonatanpaul@gmail.com>
2025-02-17 13:04:19 +0600

    learn how to check string length and string is mutable or immutable

--- string.js
+++ string.js
@@ -11,3 +11,22 @@ console.log(typeof country); // string
 console.log(typeof division); // string
 console.log(typeof district); // string
 console.log(typeof thana); // object but under the hod string
+
+// how to check array lenght
+// array is mutable
+const number = [15, 19, 18, 16, 13, 12];
+console.log(number.length); // 6
+number[0] = 16; // its working
+console.log(number); //[16, 19, 18, 16, 13, 12]
+
+// how to findout array element with index
+console.log(number[1]); // 19
+
+// note: string is immutable
+const capital = "Dhaka";
+console.log(capital);
+capital[0] = "F"; // its not working
+console.log(capital); // Dhaka
+// how to check string data length check
+console.log(capital.length);
+console.log(capital[2]);

2b06e93fc566c9e7d0be3b74e10e538c5a4a8e7c
sonatanpaul <iamsonatanpaul@gmail.com>
2025-02-17 18:58:17 +0600

    learn how to acess object property value

--- object.js
+++ object.js
@@ -5,5 +5,21 @@ const botle = {
   Price: 45,
   isClean: true,
 };
-
 console.log(botle);
+
+// how to check object property value
+const person = {
+  name: "Sonatan",
+  age: 25,
+  profession: "Web Developer",
+  married: true,
+};
+
+// dot notation
+const personName = person.name;
+console.log(personName); // Sonatan
+
+// bracket notation
+const personAge = person["age"];
+console.log(personAge);
+

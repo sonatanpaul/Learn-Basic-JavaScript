4ac2fbf33ea8b766b249f308fc0c320b15be27f1
sonatanpaul <iamsonatanpaul@gmail.com>
2025-02-17 23:38:37 +0600

    learn sort method and tosorted method

--- more-array.js
+++ more-array.js
@@ -17,3 +17,20 @@ while (i < friends.length) {
   i++;
 }
 
+// array reverse method
+const book = ["Bangla", "English", "Hindi", "Math"];
+console.log(book);
+const reverse = book.reverse();
+console.log(reverse);
+
+// array sort method
+const student = ["badhon", "bijoy", "akash", "zabir", "vhola"];
+console.log(student);
+const updateStudent = student.sort();
+console.log(updateStudent); // sorted method is mutable
+
+// tosorted method
+const number = [1, 2, 35, 4, 11, 22, 33, 1, 4, 44];
+console.log(number);
+const updateNumber = number.toSorted((a, b) => a - b);
+console.log(updateNumber); // toSorted method is immutable

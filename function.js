e7ba939a64514d3fca9a33ffc30d88c74a0fb406
sonatanpaul <iamsonatanpaul@gmail.com>
2025-02-18 19:01:31 +0600

    learn javascript function return keyword

--- function.js
+++ function.js
@@ -6,9 +6,40 @@ function myFunction() {
 myFunction();
 
 // learn function parameter and argument
+// single parameter
 function square(number) {
   const multi = number * number;
   console.log(multi);
 }
 
 square(6);
+
+// multi parameter
+function add(num1, num2) {
+  const sum = num1 + num2;
+  console.log(sum);
+}
+
+add(5, 6);
+
+// odd number function
+function odd(num1, num2) {
+  const result = num1 - num2;
+  console.log(result);
+}
+
+const num1 = 520;
+const num2 = 220;
+odd(num1, num2);
+
+// learn javascript function return keyword
+
+function tenTimes(number) {
+  const result = number * 10;
+  return result;
+}
+
+tenTimes(10);
+
+const output = tenTimes(15);
+console.log(output);
